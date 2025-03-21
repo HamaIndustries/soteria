2ca327fbfacf7a5d534828c0313d512f6793f108
hama <hama@hama.industries>
2025-03-20 22:00:17 -0700

    add lance to item group

--- src/main/java/symbolics/division/soteria/SoterianItems.java
+++ src/main/java/symbolics/division/soteria/SoterianItems.java
@@ -1,13 +1,19 @@
 package symbolics.division.soteria;
 
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
 import symbolics.division.soteria.item.SoterianLance;
+import symbolics.division.spirit_vector.SpiritVectorMod;
 
 public class SoterianItems {
-    public static void init() {
-    }
-
     public static final Item SOTERIAN_LANCE = Registry.register(Registries.ITEM, Soteria.id("soterian_lance"), new SoterianLance());
+
+    public static void init() {
+        ItemGroupEvents.modifyEntriesEvent(RegistryKey.of(RegistryKeys.ITEM_GROUP, SpiritVectorMod.id("item_group")))
+                .register(content -> content.add(SOTERIAN_LANCE.getDefaultStack()));
+    }
 }
